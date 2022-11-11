70f3e52a23b2f46a1d4c9affcb0fc587e91f9250
Christian <caldanas2@miumg.edu.gt>
2022-11-11 16:18:35 -0600

    LOGIN FUNCIONAL CHRISTIAN ALDANA

---
+++ src/java/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author christian
+ */
+public class Conexion {
+    private String url="jdbc:mysql://localhost:3306/universidad";//url de MySQL
+    private String usuario="root";
+    private String clave="Root"; 
+    private Connection conexion=null;  
+    
+    public Connection conectar(){   
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion=DriverManager.getConnection( url, usuario,clave);
+        }  catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {   
+           ex.printStackTrace();
+        }         
+        return conexion;        
+    }
+   
+}
